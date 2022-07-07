958860b9b1000987a6b824d89300dfa28bd2ed85
ckh7488 <msj74881@gmail.com>
2022-07-07 21:59:50 +0900

    Fix : 그림 보이기 및 그림추가 부분 수정

--- pages/createnft/index.js
+++ pages/createnft/index.js
@@ -1,24 +1,51 @@
 import ImageLoader from "../../components/ImageLoader/ImageLoader";
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-import { IconButton } from "@mui/material";
+import { Button, Card, CardContent, CardMedia, Divider, IconButton, Input, Typography } from "@mui/material";
 import { useState } from "react";
 
-const createnftpage = () => {
-    const [imgLoaderNum, setImgLoaderNum] = useState([0]);
 
-    const handleOnClickX = (componentId)=>{
-        const myArr = imgLoaderNum.filter((el)=>el !== componentId);
-        setImgLoaderNum(myArr);
+const createnftpage = () => {
+    const [attributeTabs, setAttributesTabs] = useState([1]);
+    const [selectedImgs, setSelectedImgs] = useState([]);
+
+    const handleImgClick = (imgUrl,isBackGround=false) => {
+        const A = selectedImgs.filter(e => e === imgUrl);
+        // console.log(A);
+        A.length > 0 ? setSelectedImgs([...selectedImgs.filter(e => e !== imgUrl)]) :
+        isBackGround ? setSelectedImgs([imgUrl, ...selectedImgs]) : setSelectedImgs([...selectedImgs, imgUrl]) 
+    }
+
+    const resetDisplay = () => {
+        setSelectedImgs([]);
+    }
+
+    const removeOneTap = (tabId)=>{
+        const tmpArr = attributeTabs.filter(e=>tabId!==e);
+        setAttributesTabs(tmpArr);
     }
 
     return (
-        <div className="container">
-            createnft page
-            <div className="containerH">
-                { imgLoaderNum.map((_,idx)=><ImageLoader key={imgLoaderNum[idx]} handleOnX={()=>{handleOnClickX(imgLoaderNum[idx])}}></ImageLoader>) }
-                <IconButton onClick={ ()=>{setImgLoaderNum([...imgLoaderNum, imgLoaderNum.slice(-1)[0]+1 ])} }>
-                    <AddBoxOutlinedIcon></AddBoxOutlinedIcon>
-                </IconButton>
+        <div className="container" style={{ padding: '15px', justifyContent: 'flex-start' }}>
+            <div>
+                <Button onClick={resetDisplay}>reset</Button>
+                <Card style={{marginBottom:'15px'}}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Displayed Sample Image
+                        </Typography>
+                    </CardContent>
+                    <Divider style={{ width: '100%',}} ></Divider>
+                    <div className="displaySelectedContainer">
+                        {selectedImgs.map(imgurl => {
+                            return <img src={imgurl} key={imgurl} style={{ height: '300px', width: '300px', position: "absolute", objectFit:"contain" }}  ></img>
+                        })}
+                    </div>
+                </Card>
+            </div>
+            <Divider style={{ width: '100%' }}></Divider>
+            <div style={{marginTop:'15px'}}>
+                <Button onClick={() => { setAttributesTabs([...attributeTabs, attributeTabs.slice(-1)[0] + 1]) }}>Add attribute</Button>
+                {attributeTabs.map((el, idx) => <ImageLoader key={attributeTabs[idx]} handleImgClick={handleImgClick} resetDisplay={resetDisplay} isBackGround={el} handleClickX={removeOneTap} />)}
             </div>
         </div>
     )
