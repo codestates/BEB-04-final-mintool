99f78c4b25ed261e0fae02a1650a42a7b5ae899d
ckh7488 <msj74881@gmail.com>
2022-07-17 22:36:42 +0900

    Feat : mypage 에서 nft 불러오기 추가

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -2,21 +2,36 @@ import { TextField } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
-const MyPage : NextPage = ()=>{
+const MyPage: NextPage = () => {
     const [projectName, setProjectName] = useState<string>('');
     const [metaDataArr, setMetaDataArr] = useState<Array<any>>([]);
-    
 
-    const handleEnter = ()=>{
-        fetch(`/api/fs/${projectName}`)
+
+    const handleEnter = () => {
+        const ret =
+            fetch(`/api/fs/${projectName}`, { method: "POST", })
+                .then(r => r.json())
+                .then(x => { console.log(x, Object.keys(x.meta), Object.values(x.meta)); setMetaDataArr(Object.values(x.meta)); return x; })
+                // .then(rj => setMetaDataArr(Object.values(rj.meta)))
+
     }
 
     return (
-        <TextField 
-            onChange={(e)=>{setProjectName(e.target.value)}}
-            onKeyUp={(e)=>{if(e.key === 'Enter'){handleEnter()}}}
-            value={projectName}
-            label="ProjectName" />
+        <div>
+            <TextField
+                onChange={(e) => { setProjectName(e.target.value) }}
+                onKeyUp={(e) => { if (e.key === 'Enter') { handleEnter() } }}
+                value={projectName}
+                label="ProjectName" />
+
+            {metaDataArr.map(metaData => {
+                return (
+                    <div>
+                        <span>{metaData.index}</span>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
