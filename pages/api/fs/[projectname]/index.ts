0815308e62bdfd251320bcaf39ce841f2705ed18
ckh7488 <msj74881@gmail.com>
2022-07-18 05:48:00 +0900

    Fix : response only metadata

--- pages/api/fs/[projectname]/index.ts
+++ pages/api/fs/[projectname]/index.ts
@@ -19,12 +19,12 @@ export default async function handler(
         const myImg : any = await myClient.db(projectname).collection('img').find().toArray();
         const myMeta = await myClient.db(projectname).collection('meta').find().toArray();
         // const retImg : Buffer = tmp?.img?.buffer;
-        const ImgArr =
-        Object.keys(myImg).map((imgObjIdx : string) =>{
-            return myImg[imgObjIdx].img?.buffer;
-        })
+        // const ImgArr =
+        // Object.keys(myImg).map((imgObjIdx : string) =>{
+        //     return myImg[imgObjIdx].img?.buffer;
+        // })
         
-        res.send({img : ImgArr, meta : myMeta});
+        res.send({img : null, meta : myMeta});
         // const retImg : Buffer = tmp?.img?.buffer;
         // res.send(retImg.toString('base64'));
     }
