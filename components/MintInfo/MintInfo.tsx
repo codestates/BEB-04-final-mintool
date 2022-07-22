2f14b9157b127ab693261df955ed5e7559454803
ckh7488 <msj74881@gmail.com>
2022-07-22 10:30:34 +0900

    Feat : mypage nft names

--- components/MintInfo/MintInfo.tsx
+++ components/MintInfo/MintInfo.tsx
@@ -1,26 +1,41 @@
 // import Caver from 'caver-js'
 import { useEffect, useState } from 'react';
 
+
 declare let caver: any;
-declare let klaytn : any;
+declare let klaytn: any;
 
 const MintInfo = () => {
     const [blockNum, setBlockNum] = useState(0);
     const [address, setAddress] = useState<string>('');
-    // const caver = new Caver(new Caver.providers.HttpProvider('https://public-node-api.klaytnapi.com/v1/baobab'));
+    const [contractArr, setContractArr] = useState<Array<string>>([]);
+    
 
     useEffect(() => {
         const handleSetinterval = setInterval(() => { caver.klay.getBlockNumber().then((bn: number) => setBlockNum(bn)) }, 1000);
-        klaytn.enable().then((r:any)=>setAddress(r[0]))
+        klaytn.enable()
+            .then((r: any) => { setAddress(r[0]); return r[0] })
+            .then((myAddress: string) => {
+                fetch('/api/usernftnames', { method: 'POST', body: JSON.stringify({address : myAddress}) })
+                .then(r=>r.json())
+                .then(nftObj=>setContractArr(nftObj.nftNames));
+            })
+        //fetch에서 address만 보낼 경우, 문제가 발생하긴한다. (보안이슈) jwt 사용해보자 나중에.
         return () => {
             clearInterval(handleSetinterval);
         }
-    })
+    },[])
+
 
     return (
         <div>
 
-        <form  action='/api/mypage' method='post' target="_blank">
+            <form action='/api/mypage' method='post' target="_blank">
+                {contractArr.map(e=>
+                        <div>
+                            {e}
+                        </div>
+                    )}
                 <div>BlockNum is {blockNum}</div>
                 <div>Mint started at BlockNum : <input type="text" name="bn"></input></div>
                 <div>Mint price : <input type="text" name="price"></input></div>
@@ -29,7 +44,13 @@ const MintInfo = () => {
             </form>
         </div>
 
-                )
+    )
 }
 
-         export default MintInfo
+export default MintInfo
+
+
+
+
+        //  const myMongoDB = await promiseClinet;
+        //     await myMongoDB.db('users').collection(`${myAccount}`).find().toArray().then(console.log)
