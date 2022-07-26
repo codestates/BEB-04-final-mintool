6c5b7df65c5faf13d2808bd7408ecea1403bd06d
ckh7488 <msj74881@gmail.com>
2022-07-26 20:31:29 +0900

    promisify img

--- pages/api/fs/[projectname]/img/[imgid].ts
+++ pages/api/fs/[projectname]/img/[imgid].ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import promiseClinet from "../../../../../lib/mongodb";
 
 
-export default async function handler(
+export default function handler(
     req: NextApiRequest,
     res: NextApiResponse) {
     const { projectname, imgid } = req.query;           // 둘다 string 으로 들어온다. imgid 는 나중에 number값으로 넣어줘야 정상작동하더라.
@@ -13,13 +13,19 @@ export default async function handler(
     // console.log(projectname, imgid);
     // res.send('hi');
     if (typeof projectname === 'string' && typeof imgid === 'string') {
-        const myClient = await promiseClinet;
-        const tmp = await myClient.db(projectname).collection('img').findOne({index : parseInt(imgid)});
-        const retImg : Buffer = tmp?.img?.buffer;
+        promiseClinet.then(myClient =>{
+            myClient.db(projectname).collection('img').findOne({index : parseInt(imgid)})
+            .then(pms=> pms?.img?.buffer)
+            .then(r=>{res.write(r); res.end();})
+        })
+        // const tmp = await myClient.db(projectname).collection('img').findOne({index : parseInt(imgid)});
+        // const retImg : Buffer = tmp?.img?.buffer;
         
-        res.write(retImg);
-        res.end();
-        // res.send(retImg.toString('base64'));
+        // res.write(retImg);
+        // res.end();
+        
+  
+       
     }
     else { res.send({message : 'wrong access!'}); return; }
 
