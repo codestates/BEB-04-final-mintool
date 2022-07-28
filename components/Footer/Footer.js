f2433520828a9a433df2da3b889dc7e97c3c80a9
ckh7488 <msj74881@gmail.com>
2022-07-28 09:22:20 +0900

    Feat : pagination for mypage

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
     return (
         <footer>
             <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-                <Typography variant="h4">Contributer Git</Typography>
+                <Typography variant="h4" fontFamily="Fantasy">Contributer Git</Typography>
                 <IconButton><Link href="https://github.com/ckh7488"><div><GitHubIcon />ckh7488</div></Link></IconButton>
                 <IconButton><Link href="https://github.com/bing72"><div><GitHubIcon />bing74</div></Link></IconButton>
             </div>
--- pages/api/fs/[projectname]/index.ts
+++ pages/api/fs/[projectname]/index.ts
@@ -6,27 +6,23 @@ import promiseClinet from "../../../../lib/mongodb";
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse) {
-    const { projectname} = req.query;           // 둘다 string 으로 들어온다. imgid 는 나중에 number값으로 넣어줘야 정상작동하더라.
-   
-    // db access auth code neeeeeded!!  
-    //
+    const { projectname } = req.query;           // 둘다 string 으로 들어온다. imgid 는 나중에 number값으로 넣어줘야 정상작동하더라.
+    const {pageNum, numShow, total} = JSON.parse(req.body);
+    console.log("pageNum and numShow and total ",pageNum, numShow, total);
 
-    
-    // res.send('hi');
     if (typeof projectname === 'string') {
         const myClient = await promiseClinet;
+        try{
+        // const myImg : any = await myClient.db(projectname).collection('img').find().limit(numShow).skip(pageNum-1).toArray();
+        const myMeta = await myClient.db(projectname).collection('meta').find().limit(numShow).skip((pageNum-1)*numShow).toArray();
+        let totalCount = null;
+        if(total) totalCount = await myClient.db(projectname).collection('meta').estimatedDocumentCount();
         
-        const myImg : any = await myClient.db(projectname).collection('img').find().toArray();
-        const myMeta = await myClient.db(projectname).collection('meta').find().toArray();
-        // const retImg : Buffer = tmp?.img?.buffer;
-        // const ImgArr =
-        // Object.keys(myImg).map((imgObjIdx : string) =>{
-        //     return myImg[imgObjIdx].img?.buffer;
-        // })
+        // console.log(totalCount)
         
-        res.send({img : null, meta : myMeta});
-        // const retImg : Buffer = tmp?.img?.buffer;
-        // res.send(retImg.toString('base64'));
+        res.send({total : totalCount, meta : myMeta});
+        }
+        catch(e) {  res.send({message:`db error : ${e}`}); return;  }
     }
     else { res.send({message : 'wrong access!'}); return; }
 
